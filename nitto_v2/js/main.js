cbd0ea1a29966298b814d84c5a58f63d29663689
aladorden <>
2018-06-11 11:30:05 +0300

    rain2

--- nitto_v2/js/main.js
+++ nitto_v2/js/main.js
@@ -3367,7 +3367,7 @@
     },
     "rainTyreAlter": function() {
       $(document).ready(function() {
-        var image = document.getElementById('background-rain');
+        var image = document.getElementsByClassName('images-compare-before');
         new RainyDay({
           image: image,
           enableSizeChange: true,
